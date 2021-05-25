fbd47c438ca070db88f301e73ad8fb27ef4843fd
Peter Ullrich <PJUllrich@users.noreply.github.co>
2021-05-25 20:47:40 +0200

    Remove umbrella project

--- README.md
+++
@@ -1,4 +0,0 @@
-# Radinfo
-
-**TODO: Add description**
-
--- apps/proxy/test/proxy_web/routing_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule ProxyWeb.RoutingTest do
-  use ProxyWeb.ConnCase, async: true
-
-  test "routes to the correct endpoints", %{conn: conn} do
-    assert true
-  end
-end
